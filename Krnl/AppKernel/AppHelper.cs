cdfc72b9784963f162a474e7b975da9322d08780
virtualtoja <76951241+virtualtoja@users.noreply.github.com>
2021-05-18 10:09:36 +0200

    Add files via upload

---
+++ Krnl/AppKernel/AppHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramManager.Krnl.AppKernel
+{
+    public static class AppHelper
+    {
+        public static void RunSystemCodes(string txt)
+        {
+            
+        }
+    }
+}
